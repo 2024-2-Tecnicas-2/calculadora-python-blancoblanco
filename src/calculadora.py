60bf108f3fb7169e63114953cb5c483caea94952
limon <kilo@gmail.com>
2024-08-21 19:26:36 -0500

    pruebas unitarias

--- src/calculadora.py
+++ src/calculadora.py
@@ -13,11 +13,14 @@ def calcular(numero1, numero2, operacion):
         case '/':
             # Operación división
             respuesta = numero1 / numero2
-
-        # TODO: DEBES COLOCAR TU CÓDIGO AQUÍ# TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO, COMPROBANDO LOS SÍMBOLOS ^ Y %.
-
+        case'^':
+            respuesta=pow(numero1,numero2)  
+        case'%':
+            respuesta=numero1%numero2    
         case _:
             raise ValueError("Operación inválida.")
+
+        # TODO: DEBES COLOCAR TU CÓDIGO AQUÍ# TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO, COMPROBANDO LOS SÍMBOLOS ^ Y %.
 
     return respuesta
 
@@ -28,7 +31,7 @@ if __name__ == '__main__':
     print("Ingrese el número 2")
     numero2 = int(input())
     # TODO: EN LA SIGUIENTE LÍNEA DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA QUE LE APAREZCA AL USUARIO.
-    print("Ingrese la operación (+, -, *, /)")
+    print("Ingrese la operación (+, -, *, /,^ ,%)")
     operacion = input()
 
     resultado = calcular(numero1, numero2, operacion)
